ef16efd01c515b20155c9e4972689754ad68312c
mingwhsun <mingwhsun@thoughtworks.com>
2019-05-07 09:57:49 +0800

    [1] complete case should_throw_no_available_exception_when_park_given_not_available_position_and_unique_number

--- src/main/java/com/parkingLot/ParkingLot.java
+++ src/main/java/com/parkingLot/ParkingLot.java
@@ -1,18 +1,28 @@
 package com.parkingLot;
 
+import com.exceptions.NoAvailableException;
 import com.exceptions.NoNumberException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ParkingLot {
     private int capacity;
+    private Map<String, Car> cars;
 
     public ParkingLot(int capacity) {
         this.capacity = capacity;
+        cars = new HashMap<>();
     }
 
-    public ParkingTicket park(Car car) throws NoNumberException {
+    public ParkingTicket park(Car car) throws Exception {
+        if (cars.size() >= capacity) {
+            throw new NoAvailableException();
+        }
         if (car.getNumber() == null) {
             throw new NoNumberException();
         }
+        cars.put(car.getNumber(), car);
         return new ParkingTicket(car.getNumber());
     }
 }
--- src/test/java/com/parkingLot/ParkingLotTest.java
+++ src/test/java/com/parkingLot/ParkingLotTest.java
@@ -1,5 +1,6 @@
 package com.parkingLot;
 
+import com.exceptions.NoAvailableException;
 import com.exceptions.NoNumberException;
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ParkingLotTest {
     @Test
-    void should_return_ticket_when_park_given_available_position_and_unique_number() throws NoNumberException {
+    void should_return_ticket_when_park_given_available_position_and_unique_number() throws Exception {
         ParkingLot parkingLot = new ParkingLot(1);
         ParkingTicket ticket = parkingLot.park(new Car("A12345"));
         assertEquals("A12345", ticket.getId());
@@ -18,5 +19,12 @@ public class ParkingLotTest {
     void should_throw_no_number_exception_when_park_given_available_position_and_no_number() {
         ParkingLot parkingLot = new ParkingLot(1);
         assertThrows(NoNumberException.class,() -> parkingLot.park(new Car()));
+    }
+
+    @Test
+    void should_throw_no_available_exception_when_park_given_not_available_position_and_unique_number() throws Exception {
+        ParkingLot parkingLot = new ParkingLot(1);
+        parkingLot.park(new Car("B12345"));
+        assertThrows(NoAvailableException.class,() -> parkingLot.park(new Car("A1123")));
     }
 }
