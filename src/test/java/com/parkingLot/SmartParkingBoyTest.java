77c1b9b6545d517673cce77add65b06ad79beca9
mingwhsun <mingwhsun@thoughtworks.com>
2019-05-08 23:35:40 +0800

    [3] compete case should_throw_duplicated_car_exception_when_park_given_available_position_in_park_lot_b_and_available_position_in_park_lot_a_and_duplicated_number_in_parking_lot_a

--- src/test/java/com/parkingLot/SmartParkingBoyTest.java
+++ src/test/java/com/parkingLot/SmartParkingBoyTest.java
@@ -1,6 +1,7 @@
 package com.parkingLot;
 
 import com.SmartParkingBoy;
+import com.exceptions.DuplicatedCarException;
 import com.exceptions.NoNumberException;
 import org.junit.jupiter.api.Test;
 
@@ -45,5 +46,17 @@ public class SmartParkingBoyTest {
         SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
 
         assertThrows(NoNumberException.class,() -> smartParkingBoy.park(new Car()));
+    }
+
+    @Test
+    void should_throw_duplicated_car_exception_when_park_given_available_position_in_park_lot_b_and_available_position_in_park_lot_a_and_duplicated_number_in_parking_lot_a() throws Exception {
+        ParkingLot parkingLotA = new ParkingLot(2, "A");
+        parkingLotA.park(new Car("A12345"));
+        Map<String, ParkingLot> parkingLots = new HashMap<>();
+        parkingLots.put("A", parkingLotA);
+        parkingLots.put("B", new ParkingLot(1, "B"));
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+
+        assertThrows(DuplicatedCarException.class,() -> smartParkingBoy.park(new Car("A12345")));
     }
 }
