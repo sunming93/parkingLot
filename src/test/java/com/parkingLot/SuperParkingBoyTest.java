cbd13f459f11382a92a0af7bd16de42945df2ca1
mingwhsun <mingwhsun@thoughtworks.com>
2019-05-09 12:59:10 +0800

    [4] complete case should_throw_no_number_exception_when_park_given_available_position_in_park_lot_b_and_available_position_in_park_lot_a_and_no_number

--- src/test/java/com/parkingLot/SuperParkingBoyTest.java
+++ src/test/java/com/parkingLot/SuperParkingBoyTest.java
@@ -1,12 +1,15 @@
 package com.parkingLot;
 
+import com.SmartParkingBoy;
 import com.SuperParkingBoy;
+import com.exceptions.NoNumberException;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SuperParkingBoyTest {
     @Test
@@ -37,5 +40,15 @@ public class SuperParkingBoyTest {
 
         assertEquals("A12345", ticket.getId());
         assertEquals("B",ticket.getParkingLotName());
+    }
+
+    @Test
+    void should_throw_no_number_exception_when_park_given_available_position_in_park_lot_b_and_available_position_in_park_lot_a_and_no_number() throws Exception {
+        Map<String, ParkingLot> parkingLots = new HashMap<>();
+        parkingLots.put("A", new ParkingLot(1, "A"));
+        parkingLots.put("B", new ParkingLot(1, "B"));
+        SuperParkingBoy superParkingBoy = new SuperParkingBoy(parkingLots);
+
+        assertThrows(NoNumberException.class,() -> superParkingBoy.park(new Car()));
     }
 }
